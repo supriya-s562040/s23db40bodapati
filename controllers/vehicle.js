33f65c02b0734719ee1a817aeddc85f6973c7a11
Supriya Bodapati <supriyabodapati94@gmail.com>
2023-04-19 00:29:13 -0500

    ss7

--- controllers/vehicle.js
+++ controllers/vehicle.js
@@ -122,3 +122,16 @@ exports.vehicle_view_one_Page = async function(req, res) {
     res.send(`{'error': '${err}'}`);
     }
     };
+    // Handle building the view for creating a vehicle.
+// No body, no in path parameter, no query.
+// Does not need to be async
+exports.vehicle_create_Page = function(req, res) {
+    console.log("create view")
+    try{
+    res.render('vehiclecreate', { title: 'vehicle Create'});
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
--- routes/vehicle.js
+++ routes/vehicle.js
@@ -6,4 +6,6 @@ var router = express.Router();
 router.get('/', vehicle_controlers.vehicle_view_all_Page );
 /* GET detail vehicle page */
 router.get('/detail', vehicle_controlers.vehicle_view_one_Page);
+/* GET create vehicle page */
+router.get('/create', vehicle_controlers.vehicle_create_Page);
 module.exports = router;
