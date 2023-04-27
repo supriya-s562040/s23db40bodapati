bc6aa27c72f4a1aa06a4ceb2d4eca0ba7be0791b
Supriya Bodapati <supriyabodapati94@gmail.com>
2023-04-27 02:24:18 -0500

    Bonus

--- models/vehicle.js
+++ models/vehicle.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 const vehicleSchema = mongoose.Schema({
-company: String,
-price: Number,
-color: String
+company: {type:String, minlength:4,maxlength:25},
+price: {type:Number, min:3,max:10000},
+color: {type:String,minlength:2,maxlength:1000},
 })
 module.exports = mongoose.model("vehicle",vehicleSchema)
--- routes/vehicle.js
+++ routes/vehicle.js
@@ -4,14 +4,21 @@ const vehicle_controlers= require('../controllers/vehicle');
 var router = express.Router();
     /* GET vehicle */
 router.get('/', vehicle_controlers.vehicle_view_all_Page );
+const secured = (req, res, next) => {
+    if (req.user){
+    return next();
+    }
+    req.session.returnTo = req.originalUrl;
+    res.redirect("/login");
+}
 /* GET detail vehicle page */
 router.get('/detail', vehicle_controlers.vehicle_view_one_Page);
 /* GET create vehicle page */
-router.get('/create', vehicle_controlers.vehicle_create_Page);
+router.get('/create',secured, vehicle_controlers.vehicle_create_Page);
 /* GET create update page */
 router.get('/update', vehicle_controlers.vehicle_update_Page);
 /* GET delete vehicle page */
-router.get('/delete', vehicle_controlers.vehicle_delete_Page);
+router.get('/delete',secured, vehicle_controlers.vehicle_delete_Page);
 
 module.exports = router;
 
